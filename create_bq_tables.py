7069b04df85813e6808ede1b2ce58c3b914269a2
Durga Nemani <durga.nemani@yahooinc.com>
2024-09-26 14:34:57 -0400

    working for all tables

--- create_bq_tables.py
+++ create_bq_tables.py
@@ -25,38 +25,45 @@ def create_bigquery_dataset(client, dataset_id):
     dataset = client.create_dataset(dataset, exists_ok=True)  # API request
     print(f"Created dataset {client.project}.{dataset.dataset_id}")
 
-def create_genres_table():
-    # Path to your service account key file
-    key_path = 'qwiklabs-gcp-00-cc9e35073c89-02d2bd1ed53b.json'  # Replace with the path to your service account key file
+def create_table(client,table_name, dataset_id):
 
-    # Authenticate using the service account key file
-    credentials = service_account.Credentials.from_service_account_file(key_path)
-
-    print(f"Project Id {credentials.project_id}")
-
-    # Initialize a BigQuery client with the credentials
-    client = bigquery.Client(credentials=credentials, project=credentials.project_id)
-
-    # Define the dataset and table name
-    dataset_id = 'default'  # Replace with your dataset ID
-    table_id = f'{client.project}.{dataset_id}.genres'
-    
-    # Create the dataset
-    create_bigquery_dataset(client, dataset_id)
+    table_id = f'{client.project}.{dataset_id}.{table_name}'
 
     # Load the schema from the JSON file
-    schema_json = load_schema_from_file('schema/genres.json')  # Replace with the path to your genres.json file
+    schema_json = load_schema_from_file(f'schema/{table_name}.json')  # Replace with the path to your genres.json file
 
     # Create the BigQuery schema
     schema = create_bigquery_schema(schema_json)
 
     # Create a Table object
-    table = bigquery.Table( table_id, schema=schema)
+    table = bigquery.Table(table_id, schema=schema)
 
     # Create the table in BigQuery
     table = client.create_table(table)  # API request
 
     print(f"Created table {table.project}.{table.dataset_id}.{table.table_id}")
 
-# Call the function to create the table
-create_genres_table()
+
+# Path to your service account key file
+key_path = 'qwiklabs-gcp-00-cc9e35073c89-02d2bd1ed53b.json'  # Replace with the path to your service account key file
+
+# Authenticate using the service account key file
+credentials = service_account.Credentials.from_service_account_file(key_path)
+
+print(f"Project Id {credentials.project_id}")
+
+# Initialize a BigQuery client with the credentials
+client = bigquery.Client(credentials=credentials, project=credentials.project_id)
+
+# Define the dataset and table name
+dataset_id = 'default'  # Replace with your dataset ID
+
+# Create the dataset
+create_bigquery_dataset(client, dataset_id)
+
+# create a for loop to create tables in schema subfolder
+tables = ['albums', 'artists', 'customers', 'employees', 'genres', 'invoice_items', 'invoices', 'media_types', 'playlists', 'playlist_track', 'tracks']
+
+for table_name in tables:
+    create_table(client, table_name, dataset_id)
+
